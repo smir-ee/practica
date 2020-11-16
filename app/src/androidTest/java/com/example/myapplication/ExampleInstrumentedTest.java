1601020b24e24f8b8ac0ea163f8b3a3509bc76cb
hell <thealexbingo@mail.ru>
2020-11-16 17:14:06 +0600

    создал проект

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.skxrb1ud.bank;
+package com.example.myapplication;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.skxrb1ud.bank", appContext.getPackageName());
+        assertEquals("com.example.myapplication", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.skxrb1ud.bank;
+package com.example.myapplication;
 
 import org.junit.Test;
 
