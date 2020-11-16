a87581822a8144c620a4f3c9b6c7cc5100ed1909
hell <thealexbingo@mail.ru>
2020-11-17 02:00:10 +0600

    проектирование макета

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
