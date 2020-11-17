1bc7641acf3d9f6a5143203a57fcbed2695f731e
stay <stay57279@gmail.com>
2020-11-17 14:29:33 +0700

    w2

--- app/src/main/java/com/example/bank/kurs.java
+++ app/src/main/java/com/example/bank/kurs.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class bank2 extends AppCompatActivity {
+public class kurs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bank2);
+        setContentView(R.layout.activity_kurs);
     }
 }
