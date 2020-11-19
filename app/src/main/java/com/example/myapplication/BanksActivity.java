89dae240fdba8d7430ae6f8ec40f0cedde83ad48
hell <thealexbingo@mail.ru>
2020-11-20 00:30:24 +0600

    point2

--- app/src/main/java/com/example/myapplication/BanksActivity.java
+++ app/src/main/java/com/example/myapplication/BanksActivity.java
@@ -10,5 +10,6 @@ public class BanksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banks);
+        getSupportActionBar().hide();
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        TextView date = (TextView) findViewById(R.id.dataTxt);
+        String dateStr = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+        date.setText(dateStr);
+
+        final View view = findViewById(R.id.cursBtn);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewCurseActivity(v);
+            }
+        });
+    }
+
+    private void viewCurseActivity(View v){
+        Intent intent = new Intent(this, cursActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewBanksActivity(View v){
+        Intent intent = new Intent(this, BanksActivity.class);
+        startActivity(intent);
     }
 
     public void viewLoginDialog(View v){
