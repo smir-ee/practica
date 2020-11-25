7740ada98e15430bc87c627aa07986acd79d0f82
User <drjagin-vadim@rambler.ru>
2020-11-25 23:39:48 +0600

    Ready Google Maps

--- app/src/main/java/com/oat/practica/activities/BanksActivity.java
+++ app/src/main/java/com/oat/practica/activities/BanksActivity.java
@@ -2,21 +2,39 @@ package com.oat.practica.activities;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.http.HttpResponseCache;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.strictmode.Violation;
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.oat.practica.R;
 import com.oat.practica.entities.EntityBanks;
 import com.oat.practica.parsers.JsonParserUA;
@@ -24,6 +42,7 @@ import com.oat.practica.parsers.JsonParserUA;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -41,21 +60,118 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import com.google.android.gms.maps.*;
+
+
 public class BanksActivity extends AppCompatActivity {
+
+    private GoogleMap gMap;
+    private LatLng myLocation;
+    private Context context;
 
     private ArrayList<EntityBanks> entityBanks;
     private ListView listView;
+    private LocationManager locationManager;
+    private Location isHere;
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_banks);
+        context = this;
 
         String url = getResources().getString(R.string.api_UA);
         new parsingJson().execute(url);
+
     }
 
+    private void createMapView() {
+        ((MapFragment) getFragmentManager().findFragmentById(R.id.mapView)).getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                gMap = googleMap;
+
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                }
+                else{
+                    gMap.setMyLocationEnabled(true);
+                    gMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                        @Override
+                        public boolean onMyLocationButtonClick() {
+                            return false;
+                        }
+                    });
+                    gMap.setOnMyLocationClickListener(new GoogleMap.OnMyLocationClickListener() {
+                        @Override
+                        public void onMyLocationClick(@NonNull Location location) {
+
+                        }
+                    });
+                }
+
+                for (int i = 0; i < entityBanks.size(); i++){
+                    final EntityBanks banks = entityBanks.get(i);
+                    String status = "";
+
+                    if (banks.getStatus())
+                        status = getResources().getString(R.string.open);
+                    else
+                        status = getResources().getString(R.string.closed);
+
+                    gMap.addMarker(new MarkerOptions().position(banks.getPosition()).title("" + i));
+                }
+
+                gMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                    @Override
+                    public View getInfoWindow(Marker marker) {
+                        return null;
+                    }
+
+                    @Override
+                    public View getInfoContents(Marker marker) {
+                        View view = getLayoutInflater().inflate(R.layout.info_window_map, null);
+                        EntityBanks banks = entityBanks.get(Integer.parseInt(marker.getTitle()));
+                        String tee = marker.getTitle();
+
+                        TextView status = (TextView) view.findViewById(R.id.textView_status_map);
+                        if (banks.getStatus()){
+                            status.setText(getResources().getString(R.string.open));
+                            status.setTextColor(getResources().getColor(R.color.colorGreen));
+                        }
+                        else {
+                            status.setText(getResources().getString(R.string.closed));
+                            status.setTextColor(getResources().getColor(R.color.colorRed));
+                        }
+
+                        ((TextView) view.findViewById(R.id.textView_address_map)).setText(banks.getStreet());
+                        ((TextView) view.findViewById(R.id.textView_type_map)).setText(banks.getType());
+                        ((TextView) view.findViewById(R.id.textView_timework_map)).setText(banks.getTimeWork());
+
+                        return view;
+                    }
+                });
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        CameraPosition cameraPosition = new CameraPosition.Builder()
+                                .target(entityBanks.get(position).getPosition())
+                                .zoom(15)
+                                .build();
+                        gMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                    }
+                });
+            }
+        });
+    }
 
 
     //  Строки
@@ -92,10 +208,10 @@ public class BanksActivity extends AppCompatActivity {
             if (parserUA.parsingData(s))
                 entityBanks = parserUA.getBanksArrayList();
 
+            createMapView();
             customAdapter adapter = new customAdapter(getApplicationContext());
             listView = (ListView) findViewById(R.id.listView_Banks);
             listView.setAdapter(adapter);
-
         }
     }
 
@@ -109,63 +225,27 @@ public class BanksActivity extends AppCompatActivity {
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-            try {
-                if (convertView == null){
-                    LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-                    convertView = (View) inflater.inflate(R.layout.row_bank, null);
-                }
-
-                String time = "";
-                EntityBanks banks = (EntityBanks) getItem(position);
-                Calendar calendar = new GregorianCalendar();
-                calendar.setTime(new Date());
-                JSONObject jsonObject = new JSONObject(banks.getTimeWork());
-                switch (calendar.get(Calendar.DAY_OF_WEEK)){
-                    case 2:
-                        time = jsonObject.getString("mon");
-                        break;
-                    case 3:
-                        time = jsonObject.getString("tue");
-                        break;
-                    case 4:
-                        time = jsonObject.getString("wed");
-                        break;
-                    case 5:
-                        time = jsonObject.getString("thu");
-                        break;
-                    case 6:
-                        time = jsonObject.getString("fri");
-                        break;
-                    case 7:
-                        time = jsonObject.getString("sat");
-                        break;
-                    case 1:
-                        time = jsonObject.getString("sun");
-                        break;
-                }
-                String[] address = banks.getAddress().split(",");
-                String[] house = time.split(" - ");
-
-                TextView status = (TextView) convertView.findViewById(R.id.textView_status_bank);
-
-                double nowHouse = Double.parseDouble(new SimpleDateFormat("HH.mm").format(calendar.getTime()));
-                if (Double.parseDouble(house[0].replace(':','.')) < nowHouse &&
-                    nowHouse < Double.parseDouble(house[1].replace(':','.'))){
-                    status.setText(getResources().getString(R.string.open));
-                    status.setTextColor(getResources().getColor(R.color.colorGreen));
-                }
-                else {
-                    status.setText(getResources().getString(R.string.closed));
-                    status.setTextColor(getResources().getColor(R.color.colorRed));
-                }
-
-                ((TextView) convertView.findViewById(R.id.textView_worktime_bank)).setText(house[0] + "-" + house[1]);
-                ((TextView) convertView.findViewById(R.id.textView_address_bank)).setText(address[4] + ", " + address[5]);
-                ((TextView) convertView.findViewById(R.id.textView_type_bank)).setText(banks.getType());
-
-            } catch (JSONException e) {
-                e.printStackTrace();
+            if (convertView == null){
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                convertView = (View) inflater.inflate(R.layout.row_bank, null);
             }
+
+            EntityBanks banks = (EntityBanks) getItem(position);
+
+            TextView status = (TextView) convertView.findViewById(R.id.textView_status_bank);
+
+            if (banks.getStatus()){
+                status.setText(getResources().getString(R.string.open));
+                status.setTextColor(getResources().getColor(R.color.colorGreen));
+            }
+            else {
+                status.setText(getResources().getString(R.string.closed));
+                status.setTextColor(getResources().getColor(R.color.colorRed));
+            }
+
+            ((TextView) convertView.findViewById(R.id.textView_worktime_bank)).setText(banks.getTimeWork().replace(" ", ""));
+            ((TextView) convertView.findViewById(R.id.textView_address_bank)).setText(banks.getStreet());
+            ((TextView) convertView.findViewById(R.id.textView_type_bank)).setText(banks.getType());
 
             return convertView;
         }
